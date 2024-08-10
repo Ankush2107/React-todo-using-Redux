92c89b95f98bf0afdce7aa43ffad6236975124c0
Ankush2107 <ankush.kumar210701@gmail.com>
2024-08-10 18:28:36 +0530

    added some styling and ui to addTodo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import TodoList from "./components/TodoList/TodoList"
 import "./App.css";
+import AddTodo from "./components/AddTodo/AddTodo";
 function App() {
   return (
     <div>
       <h1 className="heading">Todo App</h1>
       <hr />
+      <AddTodo/>
       <TodoList/>
     </div>
   )
